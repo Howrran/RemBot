05775825c6b4f5ecd9a7c2f48f38645dc2c33959
Nikola <matviiv@gmail.com>
2020-07-08 14:33:43 +0300

    added user crud service

--- src/services/user.py
+++ src/services/user.py
@@ -1,3 +1,6 @@
+"""
+User CRUD operations in DB
+"""
 from src.models import User
 from src.config import DB
 from src.utils.decorators import transaction_decorator
@@ -5,21 +8,36 @@ from src.utils.errors import NotExist
 
 
 class UserService():
+    """
+    Class with CRUD methods
+    """
 
     @staticmethod
     @transaction_decorator
     def create(username, telegram_id, interval):
+        """
+        Create new user or return user object if already exists
+
+        :param username: str
+        :param telegram_id: int
+        :param interval: int
+        :return: user object
+        """
+        user = UserService.filter(telegram_id=telegram_id)
+
+        if user:
+            return user[0]
 
         user = User(username=username, telegram_id=telegram_id, interval=interval)
         DB.session.add(user)
         return user
 
-
     @staticmethod
     def get_by_id(user_id):
         """
         Get user by id
-        :param id:
+
+        :param id: int
         :return: user or none
         """
         user = DB.session.query(User).get(user_id)
@@ -31,13 +49,12 @@ class UserService():
         """
         Update user info in database
 
-        :param user_id:
-        :param username:
-        :param telegram_id:
-        :param interval:
-        :return:
+        :param user_id: int
+        :param username: str
+        :param telegram_id: int
+        :param interval: int
+        :return: user object
         """
-
         user = UserService.get_by_id(user_id)
 
         if username:
@@ -47,7 +64,7 @@ class UserService():
         if interval:
             user.interval = interval
 
-        DB.session.merge()
+        DB.session.merge(user)
 
         return user
 
@@ -56,7 +73,8 @@ class UserService():
     def delete(user_id):
         """
         Delete user from database
-        :param user_id:
+
+        :param user_id: int
         :return: True or None
         """
         user = UserService.get_by_id(user_id)
@@ -70,14 +88,13 @@ class UserService():
     @staticmethod
     def filter(username=None, telegram_id=None, interval=None):
         """
-        Check if user exist in database.
+        Get list of user objects by parameters
 
-        :param username:
-        :param telegram_id:
-        :param interval:
-        :return:
+        :param username: str
+        :param telegram_id: int
+        :param interval: int
+        :return: list
         """
-
         data = {}
 
         if username:
@@ -88,8 +105,4 @@ class UserService():
             data['interval'] = interval
 
         users = DB.session.query(User).filter_by(**data).all()
-
         return users
-
-# use = User(username="Johnatan", telegram_id=344, interval=10)
-print(UserService.get_by_id(4))
